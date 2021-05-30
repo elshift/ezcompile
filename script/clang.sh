058f783b59f497275dc3365081edcbe73d32a7a7
Cade Martinez <cadeemz@outlook.com>
2021-05-29 23:07:24 -0500

    temp fix for clang.sh script

--- script/clang.sh
+++ script/clang.sh
@@ -1,3 +1,1 @@
-clang++ -std=c++17 -I "C:\Users\Admin\Desktop\ezcompile\pugixml\src" -I "C:\Users\Admin\Desktop\ezcompile\src" -I "%(AdditionalIncludeDirectories)" "ezcompile.cpp"
-mkdir bin
-mv a.exe bin/ezcompile.exe
+clang++ -std=c++17 -I "..\pugixml\src" -I "..\src" -I "%(AdditionalIncludeDirectories)" "ezcompile.cpp"
